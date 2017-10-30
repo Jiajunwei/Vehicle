3d84a233ac0d7f8ddb2b150e5a5b84e4e1ba73bc
jia <jiajunweii@outlook.com>
2017-10-30 15:46:17 +0800

    delete some useless

--- VehicleOrNot/copy_data.py
+++ VehicleOrNot/copy_data.py
@@ -11,36 +11,6 @@
 import pandas as pd
 import shutil
 
-
-#正负样本在同一文件夹
-
-# src = r'D:/PreciseRecognition/Process/Raw_Data/images_train/images/'
-#
-# dst = r'./data/'
-#
-# data_csv = pd.read_csv('imageName_modelID_664.csv')
-# imageName = data_csv['imageName']
-#
-#
-# for i in imageName:
-#     shutil.copy(src+i,dst)
-
-#正负样本不在同一文件夹
-# src1 = r'D:/PreciseRecognition/Process/Raw_Data/images_train/images/'
-# src2 = 'D:/Data/negamples/negative/'
-# dst = r'./data/'
-#
-# data_csv = pd.read_csv('imageName_modelID_664_2.csv')
-# imageName_frame = data_csv['imageName']
-#
-#
-# for imageName in imageName_frame:
-#     if(imageName.split('.')[0][0] == '0'):
-#         shutil.copy(src1 + imageName, dst)
-#     else:
-#         shutil.copy(src2 + imageName, dst)
-#
-#正负样本在同一文件夹下
 src = 'D:/PreciseRecognition/Process/Raw_Data/images_train/images/'
 dst = './verify/'
 
@@ -50,16 +20,6 @@ modelID_frame = data_csv['modelID']
 
 for imageName in imageName_frame:
     shutil.copy(src + imageName, dst)
-# for i in range(len(data_csv)):
-#     if modelID_frame[i] == 86 or modelID_frame[i] == 193:
-        # print modelID_frame[i],imageName_frame[i]
-        # print imageName_frame[i].split('.')[0] + '_' + str(modelID_frame[i]) + '.jpg'
-        # shutil.copy(src + imageName_frame[i],dst)
-        # with open('train86193.txt', 'a') as f1:
-        #     f1.write("%s%s%d\n" % (imageName_frame[i], ' ', int(modelID_frame[i])))
-        #
-        # with open('test.txt', 'a') as f2:
-        #     f2.write("%s%s%d\n" % (imageName_frame[i], ' ', int(modelID_frame[i])))
 
 
 
